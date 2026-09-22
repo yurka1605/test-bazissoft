190162de91182dcaaf43a50649b254456e077f90
Litvinov Yuriy <litvinov.yuriy1996@gmail.com>
2026-09-22 12:21:06 +0400

    feat: Updated readme

--- src/app/core/storage/models/storage.ts
+++ src/app/core/storage/models/storage.ts
@@ -1,5 +1,6 @@
 import { InjectionToken } from '@angular/core';
 
+// TODO: Для поддержки асинхронных Storage, таких как IndexedDB - переписать с использованием Observable
 export interface StorageService {
   get<T>(key: string): T | null | undefined;
   set<T>(key: string, value: T): void;
